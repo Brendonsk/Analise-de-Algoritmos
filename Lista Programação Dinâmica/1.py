087d1167979fe6d376b4cd31279adb950e678d21
Brendon <brendon@gmail.com>
2019-11-24 21:32:29 -0400

    added exercise 1 and cost analysis for it

--- Lista Programação Dinâmica/1.py
+++ Lista Programação Dinâmica/1.py
@@ -2,21 +2,34 @@ def impressao(l,M):
   '''
   Parametros
   l -- Vetor contendo as palavras
-  M -- numero de caracteres numa linha
+  M -- Numero de caracteres numa linha
   '''
+  #Vetor contendo o tamanho, em caracteres, de cada palavra no vetor l
+  tam_palavr = [len(l[i]) for i in range(len(l))]
 
-  i=0
-  linha=[""]
+  #Vetor contendo cada linha da saida da funcao
+  linha = [""]
+
+  #Vetor contendo o tamanho atual de cada linha do vetor 'linha'
+  tam_linha = [0]
+  
   for k in range (len(l)):
-    if (len(linha[-1])+len(l[k])+1)>M:
-      size = len(linha[-1]) #APAGAR ISSO DEPOIS
-      linha[-1]+=str((M-len(linha[-1]))*" ")
-      linha[-1]+="size = {}".format(size)
+    if (tam_linha[-1]+tam_palavr[k])>M:
+      '''
+      Caso a insercao da proxima palavra exceda o limite de caracteres da linha atual, uma nova linha
+      e adicionada a saida, e nela a palavra atual do vetor l (l[k]) e inserida
+      '''
+      linha[-1]+=str((M-tam_linha[-1])*" ")
       linha.append(l[k]+" ")
+      tam_linha.append(tam_palavr[k]+1)
     else:
+      '''
+      Caso contrario, tal palavra e inserida na linha atual
+      '''
       linha[-1]+=(l[k]+" ")
+      tam_linha[-1]+=tam_palavr[k]+1
   return('\n'.join(linha))
 
-s = (input("Sua frase aqui: ")).split(" ")
-print(s)
-print("\n\n{}".format(impressao(s,30)))
+m = int(input("Digite a quantidade maxima de caracteres em uma linha: "))
+s = (input("Digite um paragrafo de texto: ")).split(" ")
+print("\n{}".format(impressao(s,m)))
