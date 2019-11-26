eef353422f8e85e3d059e20c51fa27f2849714a7
Brendonsk <brendonbenevides@hotmail.com>
2019-11-26 11:03:59 -0400

    [NEW][WIP] Added 2.py

---
+++ Lista Programação Dinâmica/2.py
@@ -0,0 +1,9 @@
+# funcionário
+class no:
+  def __init__(self,soc):
+    self.soc = soc # sociabilidade
+    self.esq = None # ponteiro esquerdo (filho)
+    self.dir = None # ponteiro direto (irmao)
+class arvore:
+  def __init__(self,raiz:no):
+    self.raiz:raiz
